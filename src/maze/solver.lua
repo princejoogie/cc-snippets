b5adb9c149d2828bc7986f0f746c92fe8dd35874
princejoogie <princejoogie@gmail.com>
2025-04-26 02:11:21 +0800

    commit

--- src/maze/solver.lua
+++ src/maze/solver.lua
@@ -1,11 +1,68 @@
-if turtle.getFuelLevel() == 0 then
-    turtle.refuel(64)
+local path = {}
+local reverse = {
+	F = function()
+		turtle.back()
+	end,
+	R = function()
+		turtle.turnLeft()
+	end,
+	L = function()
+		turtle.turnRight()
+	end,
+}
+
+local function tryMove()
+	if turtle.detect() then
+		turtle.turnRight()
+		table.insert(path, "R")
+		if turtle.detect() then
+			-- Turn around (left twice from current position)
+			turtle.turnLeft()
+			turtle.turnLeft()
+			table.insert(path, "L")
+			table.insert(path, "L")
+			if turtle.detect() then
+				return false -- Stuck!
+			else
+				turtle.forward()
+				table.insert(path, "F")
+			end
+		else
+			turtle.forward()
+			table.insert(path, "F")
+		end
+	else
+		turtle.forward()
+		table.insert(path, "F")
+	end
+	return true
 end
 
-for i = 1, 10 do
-    turtle.forward()
-    turtle.forward()
-    turtle.forward()
-    turtle.forward()
-    turtle.turnRight()
+-- Optional: refuel check
+if turtle.getFuelLevel() == 0 then
+	turtle.select(1)
+	turtle.refuel()
 end
+
+-- Exploration phase
+for i = 1, 100 do
+	if not tryMove() then
+		print("Turtle is stuck!")
+		break
+	end
+end
+
+-- Reverse phase
+print("Returning to start...")
+for i = #path, 1, -1 do
+	local move = path[i]
+	if move == "F" then
+		reverse.F()
+	elseif move == "R" then
+		reverse.R()
+	elseif move == "L" then
+		reverse.L()
+	end
+end
+
+print("Back at start!")
